f97dc7ea53c35c4034624756841a2dbd873b4a89
LiubovSheyda <liubovsheyda@gmail.com>
2020-03-20 15:26:41 +0700

    Update script.js

--- lesson18/script.js
+++ lesson18/script.js
@@ -1,10 +1,18 @@
 //2
+
+// неверно
 alert(/^https?/.test('https://github.com'));
+
 //4
+
+// \w+ зачем?
 alert(/\w+\.jpe?g/.test('index.jpeg'));
+
 //6
 alert(/\d{12}/.test('hge12334568390'));
+
 //8
 alert(/^\d{1,2}\.\d{1,2}\.\d{4}$/.test('23.03.2019'));
+
 //10
 alert(/^\w{3,20}\@\w{2,10}\.\w{2,10}$/.test('ksaga5@mail.ru'));
